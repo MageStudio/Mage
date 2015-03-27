6b98ede0442dd16d3beab15280f7d55e676c9b07
Marco Stagni <marcoponds@gmail.com>
2015-03-27 13:06:14 +0100

    added grid to editor

--- Editor/app/SceneManager.js
+++ Editor/app/SceneManager.js
@@ -16,19 +16,23 @@ Class("SceneManager", {
 
         this.container = document.getElementById( 'scenecontainer' );
 
-        this.camera = new THREE.PerspectiveCamera( 60, $(this.container).width() / $(this.container).height(), 1, 1000 );
-        this.camera.position.z = 500;
+        this.camera = new THREE.PerspectiveCamera( 70, $(this.container).width() / $(this.container).height(), 1, 5000 );
+        this.camera.position.set( 1000, 500, 1000 );
+        this.camera.lookAt( new THREE.Vector3( 0, 200, 0 ) );
+
 
         this.controls = new THREE.OrbitControls( this.camera );
         this.controls.damping = 0.2;
+        //setting max distance
+        this.controls.maxDistance = 4000;
         this.controls.addEventListener( 'change', app.sm.render );
 
 
         this.scene = new THREE.Scene();
-        this.scene.fog = new THREE.FogExp2( 0xcccccc, 0.002 );
+        //this.scene.fog = new THREE.FogExp2( 0xcccccc, 0.002 );
 
         // world
-
+        /*
         var geometry = new THREE.CylinderGeometry( 0, 10, 30, 4, 1 );
         var material =  new THREE.MeshLambertMaterial( { color:0xffffff, shading: THREE.FlatShading } );
 
@@ -42,11 +46,11 @@ Class("SceneManager", {
             mesh.matrixAutoUpdate = false;
             this.scene.add( mesh );
 
-        }
+        }*/
 
 
         // lights
-
+        /*
         light = new THREE.DirectionalLight( 0xffffff );
         light.position.set( 1, 1, 1 );
         this.scene.add( light );
@@ -57,28 +61,36 @@ Class("SceneManager", {
 
         light = new THREE.AmbientLight( 0x222222 );
         this.scene.add( light );
+        */
 
-
+        //attaching grid to the scene
+        this.grid = new THREE.GridHelper(1000,100);
+        this.scene.add( this.grid );
         // renderer
 
         this.renderer = new THREE.WebGLRenderer( { antialias: false } );
-        this.renderer.setClearColor( this.scene.fog.color );
+        //this.renderer.setClearColor( this.scene.fog.color );
+        this.renderer.sortObjects = false;
         this.renderer.setPixelRatio( window.devicePixelRatio );
-        this.renderer.setSize( $(this.container).width(), this.container.innerHeight );
+        this.renderer.setSize( $(this.container).width(), $(this.container).height() );
 
         this.container.appendChild( this.renderer.domElement );
 
+        //creating stats helper
         this.stats = new Stats();
         this.stats.domElement.style.position = 'absolute';
         this.stats.domElement.style.top = '0px';
         this.stats.domElement.style.zIndex = 100;
         this.container.appendChild( this.stats.domElement );
 
+        //creating transform control
         this.transformControl = new THREE.TransformControls( this.camera, this.renderer.domElement );
-        this.transformControl.addEventListener( 'change', this.render );
+        this.transformControl.addEventListener( 'change', app.sm.render );
         this.scene.add(this.transformControl);
 
+        //calling animate and render methods
         this.animate();
+        this.render();
     },
 
     render: function() {
