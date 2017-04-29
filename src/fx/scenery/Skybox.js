89dc0a80793b59e6720aa6d2e26d31e4f27ddc93
Marco Stagni <mrc.stagni@gmail.com>
2017-04-30 00:53:29 +0100

    working on skybox

--- src/fx/scenery/Skybox.js
+++ src/fx/scenery/Skybox.js
@@ -1,13 +1,10 @@
 M.fx.shadersEngine.create("Skybox", {
     instance: function(options) {
-        if (window.asModule) return false;
-
         var cubeMap = new THREE.CubeTexture( [] );
         cubeMap.format = THREE.RGBFormat;
         
-        var textureName = options.texture || "skybox",
-            texture = M.imagesEngine.get(textureName),
-            getSide = function ( x, y ) {
+        var _buildCube = function(image) {
+            var getSide = function ( x, y ) {
                 var size = 1024;
                 var canvas = document.createElement( 'canvas' );
                 canvas.width = size;
@@ -17,13 +14,22 @@ M.fx.shadersEngine.create("Skybox", {
                 return canvas;
             };
 
-        cubeMap.images[ 0 ] = getSide( 2, 1 ); // px
-        cubeMap.images[ 1 ] = getSide( 0, 1 ); // nx
-        cubeMap.images[ 2 ] = getSide( 1, 0 ); // py
-        cubeMap.images[ 3 ] = getSide( 1, 2 ); // ny
-        cubeMap.images[ 4 ] = getSide( 1, 1 ); // pz
-        cubeMap.images[ 5 ] = getSide( 3, 1 ); // nz
-        cubeMap.needsUpdate = true;
+            cubeMap.images[ 0 ] = getSide( 2, 1 ); // px
+            cubeMap.images[ 1 ] = getSide( 0, 1 ); // nx
+            cubeMap.images[ 2 ] = getSide( 1, 0 ); // py
+            cubeMap.images[ 3 ] = getSide( 1, 2 ); // ny
+            cubeMap.images[ 4 ] = getSide( 1, 1 ); // pz
+            cubeMap.images[ 5 ] = getSide( 3, 1 ); // nz
+            cubeMap.needsUpdate = true;
+        }
+
+        if (option.texture) {
+            _buildCube(options.texture);
+        } else {
+            var textureName = options.textureName || 'skybox';
+            _buildCube(M.imagesEngine.get(textureName));
+        }
+        
 
         var cubeShader = THREE.ShaderLib[ 'cube' ];
         cubeShader.uniforms[ 'tCube' ].value = cubeMap;
