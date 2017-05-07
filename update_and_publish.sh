7b4c8a6a80b2a1316ef309e6bd401185aa73db47
Marco Stagni <mrc.stagni@gmail.com>
2017-05-07 15:14:48 +0100

    update build script

--- update_and_publish.sh
+++ update_and_publish.sh
@@ -6,7 +6,7 @@ sh update.sh
 echo "2 - pushing changes to github."
 git add -A
 COMMIT_MESSAGE = "[MAGE BUILD] building mage"
-git commit -m $COMMIT_MESSAGE
+git commit -m "$COMMIT_MESSAGE"
 git push origin master
 
 echo "3 - creating new version with npm."
