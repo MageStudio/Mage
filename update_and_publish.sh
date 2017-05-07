106663d26e085ec41fdf94ca3b8cfd5fab55a27e
Marco Stagni <mrc.stagni@gmail.com>
2017-05-07 15:10:02 +0100

    still working on building process

--- update_and_publish.sh
+++ update_and_publish.sh
@@ -3,16 +3,15 @@
 echo "1 - updating examples."
 sh update.sh
 
-
-echo "2 - creating new version with npm."
-VERSION=$(npm version patch)
-VERSION=$(echo $VERSION | cut -c 2-)
-
-echo "3 - pushing changes to github."
+echo "2 - pushing changes to github."
 git add -A
-COMMIT_MESSAGE = "[MAGE BUILD] building mage $VERSION"
+COMMIT_MESSAGE = "[MAGE BUILD] building mage"
 git commit -m $COMMIT_MESSAGE
 git push origin master
+
+echo "3 - creating new version with npm."
+VERSION=$(npm version patch)
+VERSION=$(echo $VERSION | cut -c 2-)
 
 echo "4 - creating new tag opn github"
 git tag $VERSION && git push --tags
