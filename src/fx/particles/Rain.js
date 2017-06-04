96f71a092b099d968e2ea3cdf278e8f5b130a389
Marco Stagni <mrc.stagni@gmail.com>
2017-06-04 20:03:22 +0100

    adding options to rain particle system

--- src/fx/particles/Rain.js
+++ src/fx/particles/Rain.js
@@ -1,37 +1,4 @@
 M.fx.particlesEngine.create('Rain', {
-    /*
-    instance: function(options) {
-        var opts = {
-            positionStyle    : Type.CUBE,
-            positionBase     : new THREE.Vector3( 0, 200, 0 ),
-            positionSpread   : new THREE.Vector3( 600, 0, 600 ),
-
-            velocityStyle    : Type.CUBE,
-            velocityBase     : new THREE.Vector3( 0, -400, 0 ),
-            velocitySpread   : new THREE.Vector3( 10, 50, 10 ),
-            accelerationBase : new THREE.Vector3( 0, -10,0 ),
-
-            //particleTexture : THREE.ImageUtils.loadTexture( 'img/raindrop2flip.png' ),
-
-            sizeBase    : 8.0,
-            sizeSpread  : 4.0,
-            colorBase   : new THREE.Vector3(0.66, 1.0, 0.7), // H,S,L
-            colorSpread : new THREE.Vector3(0.00, 0.0, 0.2),
-            opacityBase : 0.6,
-
-            particlesPerSecond : 1000,
-            particleDeathAge   : 1.0,
-            emitterDeathAge    : 60
-        };
-
-        Object.assign(opts, options);
-
-        var system = new ParticleSystem();
-	    system.setValues(opts);
-	    system.initialize();
-
-        return system;
-    },*/
 
     instance: function(options) {
         var particleGroup = new SPE.Group({
@@ -41,27 +8,27 @@ M.fx.particlesEngine.create('Rain', {
         });
         var emitter = new SPE.Emitter({
             maxAge: {
-                value: 2
+                value: options.maxAge || 2
             },
             position: {
-                value: new THREE.Vector3(0, 0, -50),
-                spread: new THREE.Vector3( 0, 0, 0 )
+                value: options.positionValue || new THREE.Vector3(0, 0, -50),
+                spread: options.positionSpread || new THREE.Vector3( 0, 0, 0 )
             },
             acceleration: {
-                value: new THREE.Vector3(0, -10, 0),
-                spread: new THREE.Vector3( 10, 0, 10 )
+                value: options.accelerationValue || new THREE.Vector3(0, -10, 0),
+                spread: options.accelerationSpread || new THREE.Vector3( 10, 0, 10 )
             },
             velocity: {
-                value: new THREE.Vector3(0, 25, 0),
-                spread: new THREE.Vector3(10, 7.5, 10)
+                value: options.velocityValue || new THREE.Vector3(0, 25, 0),
+                spread: options.velocitySpread || new THREE.Vector3(10, 7.5, 10)
             },
             color: {
-                value: [ new THREE.Color('white'), new THREE.Color('red') ]
+                value: options.colors || [ new THREE.Color('white'), new THREE.Color('red') ]
             },
             size: {
-                value: 1
+                value: options.szie || 10
             },
-            particleCount: 2000
+            particleCount: options.particleCount || 2000
         });
         particleGroup.addEmitter( emitter );
         particleGroup.clock = new THREE.Clock();
