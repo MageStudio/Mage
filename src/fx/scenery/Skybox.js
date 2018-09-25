71ae3ada564efdaab1d33ecfb9b708bb81b25b41
Marco Stagni <mrc.stagni@gmail.com>
2018-09-26 00:30:05 +0100

    updating skybox

--- src/fx/scenery/Skybox.js
+++ src/fx/scenery/Skybox.js
@@ -1,41 +1,32 @@
-M.fx.shadersEngine.create("Skybox", {
-    instance: function(options) {
-        var cubeMap = new THREE.CubeTexture( [] );
-        cubeMap.format = THREE.RGBFormat;
+export default class Skybox {
 
-        const buildCube = (image) => {
-            var getSide = function ( x, y ) {
-                var size = 1024;
-                var canvas = document.createElement( 'canvas' );
-                canvas.width = size;
-                canvas.height = size;
-                var context = canvas.getContext( '2d' );
-                context.drawImage( image, - x * size, - y * size );
-                return canvas;
-            };
-
-            cubeMap.images[ 0 ] = getSide( 2, 1 ); // px
-            cubeMap.images[ 1 ] = getSide( 0, 1 ); // nx
-            cubeMap.images[ 2 ] = getSide( 1, 0 ); // py
-            cubeMap.images[ 3 ] = getSide( 1, 2 ); // ny
-            cubeMap.images[ 4 ] = getSide( 1, 1 ); // pz
-            cubeMap.images[ 5 ] = getSide( 3, 1 ); // nz
-            cubeMap.needsUpdate = true;
+    static get options() {
+        return {
+            textureName: {
+                name: 'texture',
+                type: 'string',
+                default: 'skybox',
+                mandatory: true
+            }
         }
+    }
+
+    constructor(imagesEngine, options) {
+        this.cubeMap = new THREE.CubeTexture( [] );
+        this.cubeMap.format = THREE.RGBFormat;
 
         if (options.texture) {
-            buildCube(options.texture);
+            this.buildCube(options.texture);
         } else {
             var textureName = options.textureName || 'skybox';
-            buildCube(M.imagesEngine.get(textureName));
+            this.buildCube(imagesEngine.get(textureName));
         }
 
+        const cubeShader = THREE.ShaderLib[ 'cube' ];
+        cubeShader.uniforms[ 'tCube' ].value = this.cubeMap;
 
-        var cubeShader = THREE.ShaderLib[ 'cube' ];
-        cubeShader.uniforms[ 'tCube' ].value = cubeMap;
 
-
-        var skyBoxMaterial = new THREE.ShaderMaterial( {
+        const skyBoxMaterial = new THREE.ShaderMaterial( {
             fragmentShader: cubeShader.fragmentShader,
             vertexShader: cubeShader.vertexShader,
             uniforms: cubeShader.uniforms,
@@ -43,20 +34,29 @@ M.fx.shadersEngine.create("Skybox", {
             side: THREE.BackSide
         });
 
-        var skyBox = new THREE.Mesh(
+        this.mesh = new THREE.Mesh(
             new THREE.BoxGeometry( 1000000, 1000000, 1000000 ),
             skyBoxMaterial
         );
-
-        return skyBox;
-    },
-
-    options: {
-        textureName: {
-            name: 'texture',
-            type: 'string',
-            default: 'skybox',
-            mandatory: true
-        }
     }
-});
+
+    buildCube(image) {
+        this.cubeMap.images[ 0 ] = this.getSide(image, 2, 1 ); // px
+        this.cubeMap.images[ 1 ] = this.getSide(image, 0, 1 ); // nx
+        this.cubeMap.images[ 2 ] = this.getSide(image, 1, 0 ); // py
+        this.cubeMap.images[ 3 ] = this.getSide(image, 1, 2 ); // ny
+        this.cubeMap.images[ 4 ] = this.getSide(image, 1, 1 ); // pz
+        this.cubeMap.images[ 5 ] = this.getSide(image, 3, 1 ); // nz
+        this.cubeMap.needsUpdate = true;
+    }
+
+    getSide(image, x, y ) {
+        const size = 1024;
+        const canvas = document.createElement( 'canvas' );
+        canvas.width = size;
+        canvas.height = size;
+        const context = canvas.getContext( '2d' );
+        context.drawImage( image, - x * size, - y * size );
+        return canvas;
+    }
+}
