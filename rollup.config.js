1057b32355ad1d2a44e3d71ff12b50b67f87891a
Marco Stagni <marcoponds@gmail.com>
2020-01-01 16:16:42 +0000

    M-44 removed template folder, updated rollup (#60)

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
-
+import { terser } from 'rollup-plugin-terser';
 
 export default {
     // input for bundle, same concept as 'entry' in webpack
@@ -26,5 +26,6 @@ export default {
                 'vivus': []
             }
         }),
+        terser()
     ]
 };
