dcfab9780009219d423f7db4eb50538573df2526
Marco Stagni <marcoponds@gmail.com>
2020-01-14 01:37:52 +0000

    M-49 restored pointlight (#67)

--- src/lights/pointLight.js
+++ src/lights/pointLight.js
@@ -1,31 +1,21 @@
 import Light from './Light';
-import LightEngine from './LightEngine';
 import SceneManager from '../base/SceneManager';
 
 import {
-    SphereGeometry,
-    MeshPhongMaterial,
     PointLight as THREEPointLight
 } from 'three';
 
-export default class LightPoint extends Light {
+export default class PointLight extends Light {
 
-    constructor({ color, intensity, distance, position, name }) {
-
+    constructor({ color, intensity, distance, name }) {
         super({ color, intensity, name });
-
-        this.geometry = new SphereGeometry(
-            LightEngine.holderRadius,
-            LightEngine.holderSegment,
-            LightEngine.holderSegment
-        );
-        this.material = new MeshPhongMaterial({color: this.color});
-        this.mesh = new Mesh(this.geometry, this.material);
         this.light = new THREEPointLight(color, intensity, distance);
 
-        this.mesh.mesh.add(this.light);
-
         SceneManager.add(this.light, this);
+    }
 
+    addHelper() {
+        this.helper = true;
+        this.addHolder();
     }
 }
