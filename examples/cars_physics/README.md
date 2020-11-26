c0d617b57b1565709e117ef43798dc853b14bf1c
Marco Stagni <mrc.stagni@gmail.com>
2020-11-26 01:46:43 +0000

    removed code from examples, added concurrelty, added worker loader for rollup, added live reload for rollup, fixed car examples, added 100 cubes

--- examples/cars_physics/README.md
+++
@@ -1,1 +0,0 @@
-# Car Physics Example
--- examples/cars_physics/index.js
+++ examples/cars_physics/index.js
@@ -1,4 +1,4 @@
-import { Level, Box, Scene, Cylinder, Controls, Models, AmbientLight, PHYSICS_EVENTS } from '../../../../dist/mage';
+import { Router, store, Level, Box, Scene, Cube, Controls, Models, AmbientLight, PHYSICS_EVENTS } from '../../dist/mage.js';
 
 export default class Intro extends Level {
 
@@ -12,6 +12,12 @@ export default class Intro extends Level {
 
     createCar(name) {
         return Models.getModel('car', { name });
+    }
+
+    createCube(size, color) {
+        const cube = new Cube(size, color);
+        cube.setPosition({ y: 15 });
+        cube.enablePhysics({ mass: 1, debug: true });
     }
 
     handleSpeedChange({ data }) {
@@ -66,5 +72,52 @@ export default class Intro extends Level {
                 restLength: 0.6
             }
         });
+
+        this.createCube(2, 0xff00ff);
     }
 }
+
+const assets = {
+    models: {
+        'car': 'assets/models/buggy.gltf',
+        'wheel': 'assets/models/wheel.gltf'
+    }
+}
+
+const config = {
+    screen: {
+        h: window ? window.innerHeight : 800,
+        w: window ? window.innerWidth : 600,
+        ratio: window ? window.innerWidth / window.innerHeight : 600 / 800,
+        frameRate: 60,
+        alpha: true,
+    },
+
+    lights: {
+        shadows: true,
+    },
+
+    physics: {
+        enabled: true,
+        path: 'dist/ammo.js',
+        gravity: { x: 0, y: -9.8, z: 0}
+    },
+
+    tween: {
+        enabled: false,
+    },
+
+    camera: {
+        fov: 75,
+        near: 0.1,
+        far: 3000000,
+    },
+};
+
+window.addEventListener('load', () => {
+    store.createStore({}, {}, true);
+
+    Router.on('/', Intro);
+
+    Router.start(config, assets);
+});
