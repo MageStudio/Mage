5bb1caff12446ac02cb0433b854bfa394b1fa1dd
Marco Stagni <m.stagni@elsevier.com>
2022-03-25 12:20:16 +0000

    TASK added a setTexture method to Element, deprecated setTextureMap, added textures example

---
+++ src/materials/helpers.js
@@ -0,0 +1,68 @@
+import { MATERIALS, TEXTURES } from "../lib/constants";
+
+const MATERIAL_TEXTURE_MAP = {
+    [MATERIALS.BASIC]: [
+        TEXTURES.ALPHA,
+        TEXTURES.AO,
+        TEXTURES.ENV,
+        TEXTURES.LIGHT,
+        TEXTURES.MAP,
+        TEXTURES.SPECULAR
+    ],
+    [MATERIALS.LAMBERT]: [
+        TEXTURES.ALPHA,
+        TEXTURES.AO,
+        TEXTURES.ENV,
+        TEXTURES.LIGHT,
+        TEXTURES.MAP,
+        TEXTURES.SPECULAR,
+        TEXTURES.EMISSIVE
+    ],
+    [MATERIALS.PHONG]: [
+        TEXTURES.ALPHA,
+        TEXTURES.AO,
+        TEXTURES.ENV,
+        TEXTURES.LIGHT,
+        TEXTURES.MAP,
+        TEXTURES.SPECULAR,
+        TEXTURES.EMISSIVE,
+        TEXTURES.BUMP,
+        TEXTURES.DISPLACEMENT,
+        TEXTURES.NORMAL
+    ],
+    [MATERIALS.DEPTH]: [
+        TEXTURES.ALPHA,
+        TEXTURES.MAP,
+        TEXTURES.DISPLACEMENT
+    ],
+    [MATERIALS.STANDARD]: [
+        TEXTURES.ALPHA,
+        TEXTURES.AO,
+        TEXTURES.ENV,
+        TEXTURES.LIGHT,
+        TEXTURES.MAP,
+        TEXTURES.EMISSIVE,
+        TEXTURES.BUMP,
+        TEXTURES.DISPLACEMENT,
+        TEXTURES.NORMAL,
+        TEXTURES.METALNESS,
+        TEXTURES.ROUGHNESS
+    ],
+    [MATERIALS.THREE_TOON]: [
+        TEXTURES.ALPHA,
+        TEXTURES.AO,
+        TEXTURES.BUMP,
+        TEXTURES.DISPLACEMENT,
+        TEXTURES.EMISSIVE,
+        TEXTURES.GRADIENT,
+        TEXTURES.LIGHT,
+        TEXTURES.MAP,
+        TEXTURES.NORMAL
+    ]
+};
+
+
+export const isTextureMapAllowedForMaterial = (materialType, textureMap) => (
+    MATERIAL_TEXTURE_MAP[materialType] &&
+    MATERIAL_TEXTURE_MAP[materialType].includes(textureMap)
+)
