12bc53f4e3061624566ff5a36797e117893bcd69
Marco Stagni <marcostagni@kallikor.ai>
2026-02-01 17:56:02 +0000

    fix: missing needsUpdate=true after setting textures at runtime

--- src/entities/Element.js
+++ src/entities/Element.js
@@ -758,6 +758,7 @@ export default class Element extends Entity {
                 texture.repeat.set(textureOptions.repeat.x, textureOptions.repeat.y);
 
                 material[textureType] = texture;
+                material.needsUpdate = true;
             };
 
             applyMaterialChange(this.getBody(), applyTextureTo);
